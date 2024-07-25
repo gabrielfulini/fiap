01a2e1983afffaa7dcebb0f186bacf9803c46907
gabrielfulini <gabri.fu@hotmail.com>
2024-07-25 18:09:10 -0300

    Aula decisao

---
+++ Aula1/Aula1/Program.cs
@@ -0,0 +1,107 @@
+﻿
+void primeiroPrograma ()
+
+{
+    Console.WriteLine("Primeira linha");
+    Console.WriteLine("segunda linha");
+    Console.Write("terceira linha");
+    Console.Write("querta linha");
+    Console.WriteLine("Quebrando\nLinha\nem\num\nsó\ncomando"); //  \n quebra a linha
+
+}
+
+void saidaDeDados ()
+{
+    Console.WriteLine("Nome = " + "Edson" + "\tIdade = " + 50 + "\tAltura =  " + 1.71); // \t efetua uma tabulação
+
+    //                                                               1      2    3
+    Console.WriteLine("Nome = {0} \tIdade = {1} \tAltura = {2}", "Edson", 50, 1.71);
+    //
+    Console.WriteLine($"Nome = {"Edson"} \tIdade = {50} \tAltura = {1.71}"); // usando o $ na frente informa as informações
+}
+
+//  texto = string
+//  inteiro = int
+// real = double
+// logica = boolean
+
+
+void variaveis()
+{
+    //declaração das variaveis
+    string nome;
+    int idade;
+    double altura;
+    //Atribuicao dos valores nas variaveis
+    nome = "Edson de Oliveira";
+    idade = 50;
+    altura = 1.71;
+
+
+    Console.WriteLine("Nome = " + nome + "\tIdade = " + idade + "\tAltura =  " + altura); // \t efetua uma tabulação
+
+    //                                                               1      2    3
+    Console.WriteLine("Nome =  \tIdade = {1} \tAltura = {2}", nome, idade, altura);
+    //
+    Console.WriteLine($"Nome = {nome} \tIdade = {idade} \tAltura = {altura}");
+}
+
+
+void entradaDeDados()
+
+{
+    int valor;
+    double salario;
+    Console.Write("Digite um numero: ");
+    valor = Convert.ToInt16(Console.ReadLine()); // ReadLine (lê um valor string)
+
+    Console.WriteLine($"Você digitou:  {valor}");
+
+    /* Readline sempre lê um valor texto (string
+     * é necessário convertermos este valor
+     * Antes de pedir a digitação do usuário, exibir uma informação sobre o que digitar
+     */
+}
+void operadoresAritmeticos()
+{
+    int a = 10, b = 3;
+    double x = 15.5, y = 1.5;
+
+    Console.WriteLine($"a = {a} b = {b} \tx = {x} y = {y}");
+    Console.WriteLine($"Soma = {a + b} | {x + y}");
+    Console.WriteLine($"Subtracao = {a - b} | {x - y}");
+    Console.WriteLine($"Multiplicação = {a * b} | {x * y}");
+    Console.WriteLine($"divisao = {a / b} | {x / y}");
+
+    Console.WriteLine($"Módulo = {a % b} | {x % y}");
+}
+
+void fechar()
+{
+    int a = 7, b = 3, c;
+
+    Console.WriteLine($"a = {a} | b = {a}");
+
+    c = a++ + b++;  // a = a + 1/
+    /* a++; soma 1 depois do ;
+     * ++a; soma 1 antes do ;*/
+
+    Console.WriteLine($"a = {a} | b = {b}  |  c = {c}");
+}
+
+//exercicios
+
+void exercicioAritmetico()
+{
+    int a = 10;
+    int b = 3;
+    double c = 20.5;
+    double d = 4.0;
+
+    Console.WriteLine(a + b * d);
+    Console.WriteLine(d / b - a);
+    Console.WriteLine(c + a % b);
+    Console.WriteLine(d * (a + b) / d);
+    Console.WriteLine(c - a % b / d);
+}
+    exercicioAritmetico();
---
+++ Aula2/Aula2/Aula2/Program.cs
@@ -0,0 +1,185 @@
+﻿
+using System;
+using System.Xml;
+
+void operadoresRelacionais()
+    
+{
+    int a = 20, b = 15;
+    Console.WriteLine($"a = {a} | b = {b}");
+    bool resp;
+    resp = a > b;
+    Console.WriteLine($"> {resp}");
+    resp = a < b;
+    Console.WriteLine($"< {resp}");
+    resp = a >= b;
+    Console.WriteLine($">= {resp}");
+    resp = a <= b;
+    Console.WriteLine($"<= {resp}");
+    resp = a == b;
+    Console.WriteLine($"== {resp}");
+    resp = a != b;
+    Console.WriteLine($"!= {resp}");
+
+     a = 10;
+     b = 10;
+    Console.WriteLine($"\na = {a} | b = {b}");
+    resp = a > b;
+    Console.WriteLine($"> {resp}");
+    resp = a < b;
+    Console.WriteLine($"< {resp}");
+    resp = a >= b;
+    Console.WriteLine($">= {resp}");
+    resp = a <= b;
+    Console.WriteLine($"<= {resp}");
+    resp = a == b;
+    Console.WriteLine($"== {resp}");
+    resp = a != b;
+    Console.WriteLine($"!= {resp}");
+}
+
+
+void treinoCondicaoSimples()
+{
+    // exercicio: dado um numero pelo usuario, calcular o seu modulo matematico:
+    // numero: 67    exibe: 67
+    //numero: -45    exibe: 45
+
+
+    // --------------- ALGORITIMO
+    int num;
+    //ler um numero
+    Console.Write("Digite um numero: ");
+    num = Convert.ToInt32(Console.ReadLine());  //67
+
+    //verificar se o numero é negativo
+    if (num < 0)
+    {
+        //bloco verdade
+        // se for tranformar em positivo
+        num = num * -1;
+    }
+    //exibir um numero
+    Console.WriteLine($"O Numero digitado = {num}");
+}
+
+
+void exercicioCondicao()
+{
+    int idade;
+    Console.Write("Insira a sua idade: ");
+    idade = Convert.ToInt32(Console.ReadLine());
+
+    if (idade >= 12)
+    {
+        Console.WriteLine("Você pode jogar");
+    }
+    else
+    {
+        Console.WriteLine("Você não pode jogar");
+     }
+}
+
+
+void exercicioCondicao2()
+{
+    double venda;
+    Console.Write("Insira o valor da venda R$ ");
+    venda = Convert.ToDouble(Console.ReadLine());
+
+    if (venda > 500)
+    {
+        Console.WriteLine($"Venda com desconto R${venda = venda * 0.88}");
+    }
+    else
+    {
+        Console.WriteLine($"Venda com desconto R${venda = venda * 0.94}");
+    }
+}
+
+void exercicioEncadeado()
+{
+    //Dado um numero pelo usuario, verificar se o numero é "positivo", "negativo" ou 'nulo'
+    //num 67    positivo
+    //num 0     nulo
+    //num -4    negativo
+
+    int num;
+    Console.Write("Numero :");
+    num = Convert.ToInt32(Console.ReadLine());
+
+
+    if (num > 0)
+    {
+        Console.WriteLine("Positivo");
+    }
+    else  if (num < 0 )
+    {
+        Console.WriteLine("Negativo");
+    }
+    else
+    {
+        Console.WriteLine("nulo");
+    }
+
+}
+
+void Switch1()
+{
+    // Exibir por extenso o dia da semana a partir de um numero
+    int dia = 8; // ao comparar numero, coloque somente o numero
+    char letra = 'a'; // ao comparar char (um caractere) coloque entre apóstrofo  '  '
+    string palavra = "Edson"; // ao compara  string, coloque entre aspas ""
+
+   
+
+    switch (dia)
+    {
+        case 1:
+            Console.WriteLine("Domingo");
+            break;
+        case 2:
+            Console.WriteLine("Segunda");
+            break;
+        case 3:
+            Console.WriteLine("Terça");
+            break;
+        case 4:
+            Console.WriteLine("Quarta");
+            break;
+        case 5:
+            Console.WriteLine("Quinta");
+            break;
+        case 6:
+            Console.WriteLine("Sexta");
+            break;
+        case 7:
+            Console.WriteLine("Sábado");
+            break;
+        default:
+            Console.WriteLine("dia inexistente"); break;
+
+    }
+}
+
+
+void Switch2()
+{
+    int a, b;
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+
+    switch ((a, b))
+    {
+        case ( > 0, > 0) when a == b:
+            Console.WriteLine($"Os valores são positivos, mas iguais: {a}");
+            break;
+        case ( > 0, > 0):
+            Console.WriteLine($"Positivos mas diferentes a = {a} e b = {b}");
+            break;
+        default:
+            Console.WriteLine($"um ou os dois são negativos mas diferentes a = {a} e b = {b}");
+            break;
+    }
+}
+
